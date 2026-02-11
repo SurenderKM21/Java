3503ee2f5f1e05522c89bf30767fc0749ece15f1
Surender K M <surenderkm11a2svv@gmail.com>
2026-02-11 20:15:00 +0530

    Java arraylist

---
+++ Collections/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<>();
+        al.add(1);
+        al.add(2);
+
+        for(int i:al)
+            System.out.println(i);
+    }
+}
