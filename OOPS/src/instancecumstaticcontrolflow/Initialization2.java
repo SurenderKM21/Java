513e9e6e330204a6bd2b84ef0a9b7ecd2f9dd56c
Surender K M <surenderkm11a2svv@gmail.com>
2026-02-12 14:03:57 +0530

    Update Initialization2.java

--- OOPS/src/instancecumstaticcontrolflow/Initialization2.java
+++ OOPS/src/instancecumstaticcontrolflow/Initialization2.java
@@ -15,6 +15,6 @@ public class Initialization2 {
     }
 
     public static void main(String[] args) {
-        Object o = new Initialization2();
+        Object o = new Initialization2  ();
     }
 }
