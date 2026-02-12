cc19714d12b950c6b7de7b4bcdf00b3bb05d699a
Surender K M <surenderkm11a2svv@gmail.com>
2026-02-12 22:23:49 +0530

    end of constructors

--- OOPS/src/constructors/Parent1.java
+++ OOPS/src/constructors/Parent1.java
@@ -4,16 +4,13 @@ import java.io.IOException;
 
 public class Parent1 {
 
-    Parent1() throws IOException{
+    private Parent1() {
 
     }
 
 }
 class Child1 extends Parent1{
-    Child1() throws Exception{
 
-    }
     public static void main(String[] args) {
-        Runtime r = Runtime.getRuntime();
     }
 }
